9b1a83415d3a755af3eb113b357ea0d0830161b8
sgiraldo62 <sgiraldo62@misena.edu.co>
2021-09-24 00:51:56 -0500

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FrontEndPrueba';
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  title = 'Angular cliente prueba';
+  searchText;
+  usuarios: any;
+  totalData : any 
+  url: string = 'https://localhost:44357/api/Usuarios';
+  error : any;
+
+  ngOnInit() {  
+    this.http.get<any>(this.url).subscribe(data => {
+      debugger;
+       this.usuarios = data;
+    },error => this.error = error);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    Ng2SearchPipeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
